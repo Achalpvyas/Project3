df546ee02e85a36fadeb0dfdaa51d8adfd911b09
Achal Vyas <55011289+Achalpvyas@users.noreply.github.com>
2020-04-07 17:36:19 -0400

    Included the code for node generation.

--- A* algorithm.py
+++ A* algorithm.py
@@ -1,3 +1,4 @@
+"""
 Path Planning for Rigid Robot using A* Algorithm
 Authors:
 Achal Vyas (avyas@umd.edu)
@@ -48,3 +49,29 @@ def main():
   RPM_2 = eval(input('Please enter the  RPM value :'))
   print('The start point you entered is:', RPM_2)
   print('')
+
+  def plot_curve(X0,Y0,Theta0,UL,UR):  
+  t=0 
+  r=0.1 
+  L=1 
+  dt=0.1
+  X1=0
+  Y1=0 
+  dtheta=0
+  Theta0=3.14*Theta0/180
+  Theta1=Theta0 
+  while t<1:
+  t=t+dt 
+  X0=X0+X1
+  Y0=Y0+Y1 
+  dx=r*(UL+UR)*math.cos(Theta1)*dt
+  dy=r*(UL+UR)*math.sin(Theta1)*dt       
+  dtheta=(r/L)*(UR-UL)*dt
+  X1=X1+dx   
+  Y1=Y1+dy
+  Theta1=Theta1+0.5*dtheta
+  plt.quiver(X0, Y0, X1, Y1,units='xy' ,scale=1,color= 'r',width =0.2, headwidth = 1,headlength=0)
+  Xn=X0+X1
+  Yn=Y0+Y1 
+  Thetan=180*(Theta1)/3.14 
+  return Xn,Yn,Thetan
